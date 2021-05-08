1c2e0f835809e4b9727747d0a2387ab2de8a2453
gywreb <longnguyenhoang91@gmail.com>
2021-05-08 16:06:38 +0700

    clean unused import

--- src/components/app/Product/CreateProduct.tsx
+++ src/components/app/Product/CreateProduct.tsx
@@ -1,26 +1,17 @@
-import { FC, ReactNode } from 'react';
-import {
-  Form,
-  Col,
-  Row,
-  Upload,
-  Input,
-  Typography,
-  Button,
-  ColProps,
-} from 'antd';
-import { useForm } from 'antd/lib/form/Form';
+import { Button, Col, Form, Input, Row, Typography } from "antd";
+import { useForm } from "antd/lib/form/Form";
+import { FC, ReactNode } from "react";
 
-interface ProductType {
-  name: string;
-  price: number;
-  instock: number;
-  description: string;
-  images: string[];
-  categories: string[];
-  isActive: string;
-  featuredImg: string;
-}
+// interface ProductType {
+//   name: string;
+//   price: number;
+//   instock: number;
+//   description: string;
+//   images: string[];
+//   categories: string[];
+//   isActive: string;
+//   featuredImg: string;
+// }
 
 const { Title, Text } = Typography;
 
@@ -49,48 +40,51 @@ const CreateProduct: FC = () => {
         onFinish={handleFinish}
         form={form}
         initialValues={{
-          name: '',
+          name: "",
         }}
-        labelAlign='left'
-        labelCol={{ span: 24 }}>
-        <Row style={{ width: '100%' }}>
+        labelAlign="left"
+        labelCol={{ span: 24 }}
+      >
+        <Row style={{ width: "100%" }}>
           <Col>
             <Form.Item
-              name='name'
+              name="name"
               label={<Label>name</Label>}
               rules={[
                 {
                   required: true,
-                  message: 'Name is required',
+                  message: "Name is required",
                 },
-                { type: 'string' },
+                { type: "string" },
                 {
                   min: 8,
-                  message: 'At least 8 character',
+                  message: "At least 8 character",
                 },
-              ]}>
+              ]}
+            >
               <Input />
             </Form.Item>
           </Col>
           <Col>
             <Form.Item
-              name='price'
+              name="price"
               label={<Label>price</Label>}
               rules={[
                 {
                   required: true,
-                  message: 'price is required',
+                  message: "price is required",
                 },
                 {
-                  type: 'number',
+                  type: "number",
                 },
-              ]}>
-              <Input type='number' />
+              ]}
+            >
+              <Input type="number" />
             </Form.Item>
           </Col>
         </Row>
         <Form.Item>
-          <Button htmlType='submit' type='primary'>
+          <Button htmlType="submit" type="primary">
             Create
           </Button>
         </Form.Item>
